9f3bd171a671c84511ae1e2073d6a9bd71ccb82b
albertluck88 <39264172+albertluck88@users.noreply.github.com>
2018-05-25 17:20:03 +0700

    beres pr function

---
+++ function.js
@@ -0,0 +1,26 @@
+/* TUGAS NO 1 */
+function shoutOut(){
+  return ("Hello Function!!")
+}
+console.log(shoutOut())
+/* TUGAS NO 2 */
+function calculateMultipy(){
+ return (num1 * num2)
+}
+var num1 = 5
+var num2 = 6
+
+var hasilPerkalian = calculateMultipy(num1, num2)
+console.log(hasilPerkalian)
+
+/*TUGAS NO 3 */
+function processSentence(){
+  return ("Nama Saya " + name.concat(" umur saya " + age + " tahun, alamat saya di " + address + " , dan saya punya hobby yaitu " + hobby))
+}
+var name = "Agus"
+var age = 30
+var address = "Jln. Malioboro, Yogjakarta"
+var hobby = "gaming!!"
+
+var fullSentence = processSentence(name,age,address,hobby)
+console.log(fullSentence)
