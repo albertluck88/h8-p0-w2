75748b988ab305211bc0ea4539c8de3a4b1d4f9f
albertluck88 <39264172+albertluck88@users.noreply.github.com>
2018-05-25 15:24:01 +0700

    sudah diperbaiki

--- exercise-1.js
+++ exercise-1.js
@@ -1,17 +1,19 @@
-var nama = "Danu"
-var peran = "tabib"
+var nama = "albert"
+var peran = "ksatria"
 
-if ( nama === "mikael" && peran === " "){
-  console.log("Halo "+nama, ",Pilih peranmu untuk memulai game!")
-}else if( nama === "Nina" && peran === "ksatria"){
+if( nama === "" && peran === ""){
+  console.log("nama harus diisi")
+}else if(peran === ""){
+  console.log("Halo" + nama + ", Pilih peranmu untuk memulai Game")
+}else if( peran === "ksatria"){
   console.log("Selamat datang di Dunia Proxytia " + nama)
-  console.log("Halo " + peran +" "+ nama, ",kamu dapat menyerang dengan senjatamu!!")
-}else if( nama === "Danu" && peran === "tabib"){
+  console.log("Halo " + peran + " "+ nama, ",kamu dapat menyerang dengan senjatamu!!")
+}else if( peran === "tabib"){
   console.log("Selamat datang di Dunia Proxytia " + nama)
   console.log("Halo" + peran + " "+ nama, ",Kamu akan membantu temanmu yang terluka")
-}else if( nama === "Zero" && peran === "penyihir"){
+}else if( peran === "penyihir"){
   console.log("Selamat datang di Dunia Proxytia" + nama)
   console.log("Halo"+ peran + " "+nama, ",Ciptakan keajaiban yang membantu kemenanganmu")
-}else {
-  console.log("Nama harus diisi!!!")
+}else{
+  console.log("tidak ada peran tersebut disini")
 }
